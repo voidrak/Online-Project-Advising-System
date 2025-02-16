246d39157e4059b344ac8dccb8415ff9a742b305
Natnael Abebe <abebenatnael455@gmail.com>
2025-02-16 22:19:59 +0300

    add relationships and fillable attributes to Comment, Feedback, Project, and User models

--- Server/app/Models/Project.php
+++ Server/app/Models/Project.php
@@ -9,4 +9,37 @@ class Project extends Model
 {
     /** @use HasFactory<\Database\Factories\ProjectFactory> */
     use HasFactory;
+
+    protected $fillable = [
+        'project_title',
+        'description',
+        'student_id',
+        'advisor_id',
+        'department',
+        'document',
+        'due_date',
+        'approved',
+    ];
+
+
+    public function student()
+    {
+        return $this->belongsTo(User::class, 'student_id');
+    }
+
+    public function advisor()
+    {
+        return $this->belongsTo(User::class, 'advisor_id');
+    }
+
+    public function feedbacks()
+    {
+        return $this->hasMany(Feedback::class);
+    }
+
+    public function comments()
+    {
+        return $this->hasMany(Comment::class);
+    }
+
 }
